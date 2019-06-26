93374601c630e17ec45a6a3c93397fe86e3449b7
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2019-06-25 21:32:28 -0300

    commit inicial da aplicação em flask para exibir o indice bovespa de acordo com um intervalo de tempo passado como parâmetro, chave da api fixada no código por enquanto,adicionando validação para o valor de tempo

---
+++ Back-end/application.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from flask import jsonify
+import requests
+import json
+from models.supportedTimeIntervals import supportedTimeIntervals
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/bvsp-intraday/<int:timeInterval>', methods=['GET'])
+def getBvspIntraDay(timeInterval : int):
+    try:
+        validateTimeIntervalValue(timeInterval)
+    except Exception as exceptionMessage:
+        return str(exceptionMessage)
+
+    parameters : str = ""
+    function : str = "TIME_SERIES_INTRADAY" #parâmetro para intraday do alpha vantage
+    symbol : str = "^BVSP" #símbolo para o Bovespa no alpha vantage
+    interval : str = supportedTimeIntervals[timeInterval]
+    outputsize : str = "full" #série completa
+    apiKey : str = "M91DUVPKE0907E85" #mudar isso para um arquivo .settings ou .env
+    #montando url para chamar a api do alpha vantage
+    parameters = "function=" + function
+    parameters += "&symbol=" + symbol
+    parameters += "&interval=" + interval
+    parameters += "&outputsize=" + outputsize
+    parameters += "&apikey=" + apiKey
+    response = requests.get('https://www.alphavantage.co/query?' + parameters)
+    jsonResponse = response.json() #em python, ao desserializar o json do response o objeto é do tipo dict
+    timeStampsData = list(jsonResponse.values())[1:]
+    return jsonify(timeStampsData)#os dados da série estão a partir do segunda valor que corresponde à chave Time Stamps 
+
+def validateTimeIntervalValue(value : int):
+    if(value not in supportedTimeIntervals):
+        raise Exception("O valor de intervalo (" + str(value) + ") é inválido. Apenas os seguintes valores são suportados: " + str(list(supportedTimeIntervals.keys())) + ".")
+
+if __name__ == '__main__':
+   app.run(debug = True, port = 5000)
---
+++ Back-end/models/supportedTimeIntervals.py
@@ -0,0 +1,6 @@
+supportedTimeIntervals : dict = {}
+supportedTimeIntervals[1]  = "1min"
+supportedTimeIntervals[5]  = "5min"
+supportedTimeIntervals[15] = "15min"
+supportedTimeIntervals[30] = "30min"
+supportedTimeIntervals[60] = "60min"
